d8c0dfe6ef5c8e14db7d5e3c9f15388778d8eb19
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-05-01 07:07:26 -0500

    Refactor, troubleshoot

--- js/Player.js
+++ js/Player.js
@@ -2,21 +2,21 @@
 
 function init() {
   createHiDPICanvas = function () {
-    let cv = document.createElement("canvas");
-    cv.width = window.innerWidth;
-    cv.height = window.innerHeight;
-    cv.style.width = window.innerWidth + "px";
-    cv.style.height = window.innerHeight + "px";
-    cv.id = "editrCanvas";
-    return cv;
+	let cv = document.createElement("canvas");
+	cv.width = window.innerWidth;
+	cv.height = window.innerHeight;
+	cv.style.width = window.innerWidth + "px";
+	cv.style.height = window.innerHeight + "px";
+	cv.id = "editrCanvas";
+	return cv;
   }
 
   readjustCanvas = function (canvas) {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvas.style.width = window.innerWidth + "px";
-    canvas.style.height = window.innerHeight + "px";
-    game.set_viewport(canvas.width, canvas.height);
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	canvas.style.width = window.innerWidth + "px";
+	canvas.style.height = window.innerHeight + "px";
+	game.set_viewport(canvas.width, canvas.height);
   }
 
   //Create canvas with the device resolution.
@@ -35,17 +35,18 @@ function init() {
   game.limit_viewport = false;
 
   window.addEventListener('resize', function () {
-    readjustCanvas(canvas);
+	readjustCanvas(canvas);
   }, false);
 
   var Loop = function () {
-    ctx.fillStyle = '#111'; // Background colour
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // Draw the game
-    game.update();
-    game.draw(ctx);
-    // Request a new animation frame
-    requestAnimationFrame(Loop);
+	if (!document.hidden) {
+		ctx.fillStyle = '#111'; // Background colour
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		game.update();
+		game.draw(ctx);		
+	}
+
+	requestAnimationFrame(Loop);
   };
 
   requestAnimationFrame(Loop);
@@ -59,31 +60,31 @@ function init() {
 
   // Configure upload
   function uploadHandler(e) {
-    var file = e.target.files[0];
-    if (!file) {
-      return;
-    }
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      var contents = e.target.result;
-      // Load the contents of the file into window.map
-      game.log("[Player] Attempting to load mapvar at " + file.name);
-      window.map = {};
-      var jsonMap = JSONfn.parse(contents);
-      for (var key in jsonMap) {
-        window.map[key] = jsonMap[key];
-      }
-      game.load_map(window.map);
-    };
-    reader.readAsText(file);
+	var file = e.target.files[0];
+	if (!file) {
+	  return;
+	}
+	var reader = new FileReader();
+	reader.onload = function (e) {
+	  var contents = e.target.result;
+	  // Load the contents of the file into window.map
+	  game.log("[Player] Attempting to load mapvar at " + file.name);
+	  window.map = {};
+	  var jsonMap = JSONfn.parse(contents);
+	  for (var key in jsonMap) {
+		window.map[key] = jsonMap[key];
+	  }
+	  game.load_map(window.map);
+	};
+	reader.readAsText(file);
   };
 
   document.addEventListener('visibilitychange', function (event) {
-    if (document.hidden) {
-        game.delta_updating = false; // Delta time is not updated when the tab is not active.
-    } else {
-        game.delta_updating = true;
-    }
+	if (document.hidden) {
+		game.delta_updating = false; // Delta time is not updated when the tab is not active.
+	} else {
+		game.delta_updating = true;
+	}
 });
 
   document.getElementById('actual-btn').addEventListener('change', uploadHandler, false);
